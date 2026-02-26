7b0002a2b3124bba49401fc191f01e5999fa173b
Sebastian Schwingenschuh <sebastianschwingii@gmail.com>
2026-02-26 12:44:40 +0100

    routes in app.ts for home, login, poker

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Home } from './home/home';
+import { Poker } from './poker/poker'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "home", component: Home
+    },
+    {
+        path: "login", component: Login
+    },
+    {
+        path: "poker", component: Poker
+    },
+    {
+        path: "", redirectTo: "home", pathMatch: "full"
+    }
+];
---
+++ frontend/src/app/poker/poker.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-poker',
+  imports: [],
+  templateUrl: './poker.html',
+  styleUrl: './poker.css',
+})
+export class Poker {
+
+}
