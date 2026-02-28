12c6ff0f73a575c0561ebe456c19ba147cec348e
VAZVictor <victor.ehrenmuelller.jensen@gmail.com>
2026-02-28 16:47:58 +0100

    ./frontend, navbar compiles

---
+++ frontend/src/app/profile-overlay/profile-overlay.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-overlay',
+  imports: [],
+  templateUrl: './profile-overlay.html',
+  styleUrl: './profile-overlay.css',
+})
+export class ProfileOverlay {
+
+}
