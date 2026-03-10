ce4a36e16cfc8052167c49fd7022736554856e3d
Sebastian Schwingenschuh <sebastianschwingii@gmail.com>
2026-03-10 15:54:29 +0100

    basic routes for components

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,26 @@ import { Login } from './login/login';
 import { Home } from './home/home';
 import { Poker } from './poker/poker';
 import { ForgotPassword } from './login/forgot-password/forgot-password';
+import { Friends } from './friends/friends';
+import { Leaderboard } from './leaderboard/leaderboard';
+import { Navbar } from './navbar/navbar';
+import { ProfileOverlay } from './profile-overlay/profile-overlay';
+import { SettingsOverlay } from './settings-overlay/settings-overlay';
+import { Shop } from './shop/shop';
 
 export const routes: Routes = [
+    //{ path: "home", component: Home },
+    //{ path: "login", component: Login },
+    //{ path: "forgot-password", component: ForgotPassword },
+    //{ path: "poker", component: Poker },
+    { path: "friends", component: Friends },
     { path: "home", component: Home },
+    { path: "leaderboard", component: Leaderboard },
     { path: "login", component: Login },
-    { path: "forgot-password", component: ForgotPassword },
+    { path: "navbar", component: Navbar },
     { path: "poker", component: Poker },
+    { path: "profile-overlay", component: ProfileOverlay },
+    { path: "settings-overlay", component: SettingsOverlay },
+    { path: "shop", component: Shop },
     { path: "", redirectTo: "login", pathMatch: "full" }
 ];
