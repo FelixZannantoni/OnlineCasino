63a2756c35f916edc24fde839da3111dfe855a8d
FelixZannantoni <f.zannantoni@students.htl-leonding.ac.at>
2026-03-10 09:01:49 +0100

    worked on player oop

--- backend/src/gameLogic/player.ts
+++ backend/src/gameLogic/player.ts
@@ -6,17 +6,12 @@ export class Player {
     private username: string;
     private displayname: string;
     private balance: number = 0;
-    private cards: Card[] = [];
-    private hasDealerChip: boolean;
-    private bet: number;
 
-    constructor(playerId: string, username: string, displayname: string, balance: number, hasDealerChip: boolean, bet: number) {
+    constructor(playerId: string, username: string, displayname: string, balance: number) {
         this.playerId = playerId;
         this.username = username;
         this.displayname = displayname;
         this.balance = balance;
-        this.hasDealerChip = hasDealerChip;
-        this.bet = bet;
     }
 
     public getPlayerId(): string
@@ -32,33 +27,6 @@ export class Player {
     public getDisplayname(): string
     {
         return this.displayname;
-    }
-
-    public getDealerChip(): boolean {
-        return this.hasDealerChip;
-    }
-
-    public setDealerChip(hasDealerChip: boolean): void {
-        this.hasDealerChip = hasDealerChip;
-    }
-
-    public getBet(): number
-    {
-        return this.bet;
-    }
-
-    public setBet(bet: number): void
-    {
-        this.bet = bet;
-    }
-
-
-
-    public addCard(card: Card, playerId: string): void {
-        if (this.playerId !== playerId) {
-            throw new Error(`Cannot add card: player ID mismatch (expected ${this.playerId}, got ${playerId})`);
-        }
-        this.cards.push(card);
     }
 
     public static xNextPlayer(players: Player[], i: number,x: number): number
@@ -84,21 +52,5 @@ export class Player {
         {
             throw new Error(`Player not found`);
         }
-    }
-
-    public static playerWithDealerChip(players: Player[]): number
-    {
-        for(let i: number = 0; i < players.length; i++)
-        {
-            if(players[i].hasDealerChip == true)
-            {
-                return i;
-            }
-        }
-        return 0;
-    }
-
-    public clearHand(): void {
-        this.cards = [];
     }
 }
