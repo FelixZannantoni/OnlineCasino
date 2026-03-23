bdba9599b341f425fe3773d680342805f57e7be7
Julian Einzinger <j.einzinger@students.htl-leonding.ac.at>
2026-03-23 12:22:51 +0100

    Implement check and bet actions in poker service and router

--- backend/src/router/poker-router.ts
+++ backend/src/router/poker-router.ts
@@ -34,7 +34,45 @@ pokerRouter.put("/fold", async (req: Request, res: Response) => {
         res.status(StatusCodes.OK).json({ message: "Fold action received" });
     } else {
         res.status(StatusCodes.NOT_FOUND).json({ message: "Invalid game-ID or player-ID" });
+    } 
+});
+
+pokerRouter.put("/check", async (req: Request, res: Response) => {
+    const [playerId, gameId]: [string, string] = [req.body.playerId, req.body.gameId];
+    if(!playerId || !gameId) {
+        res.status(StatusCodes.BAD_REQUEST).json({ error: "Missing playerId or gameId in request body" });
+        return;
     }
 
-    
+    const service: PokerService = new PokerService();
+
+    const result = await service.check(playerId, gameId);
+
+    if(result) {
+        res.status(StatusCodes.OK).json({ message: "Check action received" });
+    } else {
+        res.status(StatusCodes.NOT_FOUND).json({ message: "Invalid game-ID or player-ID" });
+    }
+});
+
+pokerRouter.put("/bet", async (req: Request, res: Response) => {
+    const [playerId, gameId, betAmount]: [string, string, number] = [req.body.playerId, req.body.gameId, req.body.betAmount];
+    if(!playerId || !gameId) {
+        res.status(StatusCodes.BAD_REQUEST).json({ error: "Missing playerId or gameId in request body" });
+        return;
+    }
+    if(!betAmount || isNaN(betAmount)) {
+        res.status(StatusCodes.BAD_REQUEST).json({ message: "Invalid bet amount" });
+        return;
+    }
+
+    const service: PokerService = new PokerService();
+
+    const result = await service.bet(playerId, gameId, betAmount);
+
+    if(result) {
+        res.status(StatusCodes.OK).json({ message: "Bet received" });
+    } else {
+        res.status(StatusCodes.NOT_FOUND).json({ message: `Something happened while tryign to bet ${betAmount}` });
+    }
 });
