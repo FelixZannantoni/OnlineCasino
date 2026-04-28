a3a2829c1a7079b7d7e2aefa742bfda660931729
felix <f.zannantoni@stundents.htl-leonding.ac.at>
2026-04-28 15:18:41 +0200

    added pressed Buttons

--- backend/src/gameLogic/slotmachinePlayer.ts
+++ backend/src/gameLogic/slotmachinePlayer.ts
@@ -2,7 +2,36 @@ import { Player } from "./player"
 
 export class slotmachinePlayer extends Player {
 
+    private pressedSpin: boolean;
+    private pressedAutoSpin: boolean;
+
+
     constructor(playerId: string, username: string, displayname: string, balance: number) {
         super(playerId, username, displayname, balance);
+        this.pressedSpin = false;
+        this.pressedAutoSpin = false;
+    }
+
+    public getPressedSpin(): boolean {
+        return this.pressedSpin;
+    }
+
+    public getPressedAutoSpin(): boolean {
+        return this.pressedAutoSpin;
+    }
+
+    public userPressedSpin(): void {
+        this.resetActions();
+        this.pressedSpin = true;
+    }
+
+    public userPressedAutoSpin(): void {
+        this.resetActions();
+        this.pressedAutoSpin = true;
+    }
+
+    private resetActions(): void {
+        this.pressedSpin = false;
+        this.pressedAutoSpin = false;
     }
 }
