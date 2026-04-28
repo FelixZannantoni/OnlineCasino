17620de42fc3e5e080a1378f09df805dbf5042f2
VAZVictor <victor.ehrenmuelller.jensen@gmail.com>
2026-04-28 14:13:09 +0200

    added quit-overlay to confirm player wants to quit mid-game

---
+++ frontend/src/app/quit-overlay/quit-overlay.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quit-overlay',
+  imports: [],
+  templateUrl: './quit-overlay.html',
+  styleUrl: './quit-overlay.css',
+})
+export class QuitOverlay {
+
+}
