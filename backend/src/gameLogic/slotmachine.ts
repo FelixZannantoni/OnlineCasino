21b9902de072d697dd72f8cbdeababbd07ddf49f
felix <f.zannantoni@stundents.htl-leonding.ac.at>
2026-05-01 21:05:51 +0200

    added Symbols and winning combis

--- backend/src/gameLogic/slotmachine.ts
+++ backend/src/gameLogic/slotmachine.ts
@@ -3,8 +3,11 @@ import { SinglePlayerGame } from "./singlePlayerGame";
 
 export class slotmachine extends SinglePlayerGame {
 
+    private slots: Symbols[][];//3,5
+
     constructor(gameId: string) {
         super(gameId);
+        this.slots = [];
     }
 
     public startGame() {
@@ -15,6 +18,7 @@ export class slotmachine extends SinglePlayerGame {
         this.spin();
     }
 
+
     public startRound() {
 
     }
@@ -22,4 +26,34 @@ export class slotmachine extends SinglePlayerGame {
     private spin() {
 
     }
+}
+
+
+/*winging combs
+[1,0] [1,1] [1,2] [1,3] [1,4]
+[0,0] [0,1] [0,2] [0,3] [0,4]
+[2,0] [2,1] [2,2] [2,3] [2,4]
+[0,0] [1,1] [2,2] [1,3] [0,4]
+[2,0] [1,1] [0,2] [1,3] [2,4]
+
+[1,0] [2,1] [2,2] [2,3] [1,4]
+[1,0] [0,1] [0,2] [0,3] [1,4]
+[2,0] [2,1] [1,2] [0,3] [0,4]
+[0,0] [0,1] [1,2] [2,3] [2,4]
+[2,0] [1,1] [1,2] [1,3] [0,4]
+*/
+
+
+
+enum Symbols{
+    "ans" = 1,
+    "zwa" = 2,
+    "drei" = 3,
+    "vier" = 4,
+    "fünf" = 5,
+    "sex" = 6,
+    "siebn" = 7,
+    "acht" = 8,
+    "neun" = 9,
+    "zehn" = 10
 }
