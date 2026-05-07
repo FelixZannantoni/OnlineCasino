620fb55b24d0f74213f383c4243e87b2b0b08508
VAZVictor <victor.ehrenmuelller.jensen@gmail.com>
2026-05-07 19:13:59 +0200

    creation of roulette component

---
+++ frontend/src/app/roulette/roulette.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-roulette',
+  imports: [],
+  templateUrl: './roulette.html',
+  styleUrl: './roulette.css',
+})
+export class Roulette {
+
+}
