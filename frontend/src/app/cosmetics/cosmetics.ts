e19338d49abdc9c7fc391661caad00b2f84b5e96
VAZVictor <victor.ehrenmuelller.jensen@gmail.com>
2026-05-15 10:13:38 +0200

    standard cosmetiic structure using svg

---
+++ frontend/src/app/cosmetics/cosmetics.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
+
+@Component({
+  selector: 'app-cosmetics',
+  imports: [],
+  templateUrl: './cosmetics.html',
+  styleUrl: './cosmetics.css',
+})
+export class Cosmetics {
+
+}
