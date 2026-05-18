6913c34fa5dd2adec535670e1cbdd996c77ff5da
VAZVictor <victor.ehrenmuelller.jensen@gmail.com>
2026-05-18 18:17:07 +0200

    added information-overlay component

---
+++ frontend/src/app/information-overlay/information-overlay.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-information-overlay',
+  imports: [],
+  templateUrl: './information-overlay.html',
+  styleUrl: './information-overlay.css',
+})
+export class InformationOverlay {
+
+}
