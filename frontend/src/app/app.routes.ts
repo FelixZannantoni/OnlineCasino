58e6afacaa547515dde4c0a2d7bd213f4ccb42e5
VAZVictor <victor.ehrenmuelller.jensen@gmail.com>
2026-05-24 10:44:05 +0200

    routes to freind and structure has been added

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,9 +6,11 @@ import { Blackjack } from './table-game/blackjack/blackjack';
 import { ForgotPassword } from './login/forgot-password/forgot-password';
 import { Shop } from './shop/shop';
 import { Slotmachine } from './slotmachine/slotmachine';
+import { Friends } from './friends/friends';
 
 export const routes: Routes = [
     { path: "home", component: Home },
+    { path: "friends", component: Friends},
     { path: "login", component: Login },
     { path: "forgot-password", component: ForgotPassword },
     { path: "poker", component: Poker },
