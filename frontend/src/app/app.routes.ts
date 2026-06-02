7f51af46c038f99d6dc55358ead2222a43c54c86
VAZVictor <victor.ehrenmuelller.jensen@gmail.com>
2026-06-02 14:33:03 +0200

    big push, forgot to commit, further small changes incoming

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,11 +6,13 @@ import { Blackjack } from './table-game/blackjack/blackjack';
 import { ForgotPassword } from './login/forgot-password/forgot-password';
 import { Shop } from './shop/shop';
 import { Slotmachine } from './slotmachine/slotmachine';
+import { Club } from './club/club';
 
 export const routes: Routes = [
     { path: "home", component: Home },
     { path: "login", component: Login },
     { path: "forgot-password", component: ForgotPassword },
+    { path: "club", component: Club },
     { path: "poker", component: Poker },
     { path: "blackjack", component: Blackjack },
     { path: "slotmachine", component: Slotmachine},
