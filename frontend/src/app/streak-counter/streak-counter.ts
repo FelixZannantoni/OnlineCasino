b92b28e2d994e32d8b25f73890f9d24ebf8646ab
VAZVictor <victor.ehrenmuelller.jensen@gmail.com>
2026-06-12 17:08:01 +0200

    streak counter component added

---
+++ frontend/src/app/streak-counter/streak-counter.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-streak-counter',
+  imports: [],
+  templateUrl: './streak-counter.html',
+  styleUrl: './streak-counter.css',
+})
+export class StreakCounter {
+
+}
