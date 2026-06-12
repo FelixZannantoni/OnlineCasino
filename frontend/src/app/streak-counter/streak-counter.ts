11494a526014da8f2a1875f90711449ced38a30e
VAZVictor <victor.ehrenmuelller.jensen@gmail.com>
2026-06-12 23:16:24 +0200

    leaderboard with implemented streak counter has been added

--- frontend/src/app/streak-counter/streak-counter.ts
+++ frontend/src/app/streak-counter/streak-counter.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, input, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-streak-counter',
   imports: [],
   templateUrl: './streak-counter.html',
   styleUrl: './streak-counter.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StreakCounter {
-
+  streak = input<number>(0);
 }
