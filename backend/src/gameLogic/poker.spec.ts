468e54286961ec8df771426142e777c6ee862037
felix <f.zannantoni@students.htl-leonding.ac.at>
2026-06-28 23:28:03 +0200

    updated poker tests

--- backend/src/gameLogic/poker.spec.ts
+++ backend/src/gameLogic/poker.spec.ts
@@ -3,14 +3,14 @@ import { PokerPlayer } from "./pokerPlayer";
 
 function testPokerStart() {
     const poker = new Poker("1", "Test Poker");
-    const p1 = new PokerPlayer("u1", "u1", "U1", 10000000);
-    const p2 = new PokerPlayer("u2", "u2", "U2", 10000000);
+    const p1 = new PokerPlayer("u1", "u1", "U1", 1000);
+    const p2 = new PokerPlayer("u2", "u2", "U2", 1000);
     poker.addPlayer(p1);
     poker.addPlayer(p2);
-    
+
     console.log("Players added:", poker.getPlayers().length);
     poker.startGameStartTimer();
-    
+
     setTimeout(() => {
         const state = poker.getGameState();
         console.log("Game state phase:", state.phase);
